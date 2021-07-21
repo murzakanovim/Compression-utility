afd289f49c8e2064a9e123f583a5e78b968272d3
islam.murzakanov <islam.murzakanov@I-MURZAKANOV>
2021-07-21 16:12:49 +0300

    The beginning of the beggining

--- zipDB/main.cpp
+++ zipDB/main.cpp
@@ -1,8 +1,12 @@
+#include <string>
+
 #include <pqxx/pqxx>
 #include <zlib.h>
 #include "compress.h"
 
 int main()
 {
+    std::string oldConnectionSetting("");
+    pqxx::connection oldConnection();
     return 0;
 }

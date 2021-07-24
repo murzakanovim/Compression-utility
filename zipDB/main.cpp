2afe68f5add84594924aeaad2c93035881dded99
islam.murzakanov <islam.murzakanov@I-MURZAKANOV>
2021-07-24 11:34:29 +0300

    Edit query

--- zipDB/main.cpp
+++ zipDB/main.cpp
@@ -30,7 +30,8 @@ int main()
     auto vec = NConsulUtils::zip(res[0][0].c_str(), calculateSizeOfString(res[0][0].c_str()));
     // надо разобраться, как теперь последоватаельность байтов в ставить в БД
     // Скорее всего, нужно подключать воркер
-    std::string addQuery = "INSERT INTO t_event (zip_event) VALUES (" + vec + ")";
+    std::string addQuery = "UPDATE t_event SET zip_event = '" + vec + "' WHERE id =" + id;
+        
     newWorker.exec(addQuery.c_str());
     auto vecUn = NConsulUtils::unzip(vec.data(), vec.size());
 }

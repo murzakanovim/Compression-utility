a8e27375d67b95ad254738f3dec64e06172eb309
islam.murzakanov <islam.murzakanov@I-MURZAKANOV>
2021-08-20 07:57:26 +0300

    Delete column info

--- zipDB/tools.h
+++ zipDB/tools.h
@@ -6,7 +6,5 @@
 #include <pqxx/row>
 #include <pqxx/transaction>
 
-#include "column_info_t.h"
-
 std::basic_string_view< std::byte > getZipString(const std::string& event);
 void executeOneNote(const pqxx::row& row, pqxx::work& newWorker);

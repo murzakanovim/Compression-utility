5378a2260987ccb466fea04010020c22a39ac47a
islam.murzakanov <islam.murzakanov@I-MURZAKANOV>
2021-08-24 08:01:57 +0300

    Move connection in execute

--- zipDB/main.cpp
+++ zipDB/main.cpp
@@ -10,14 +10,21 @@
 #include "tools.h"
 #include "compress.h"
 
-//что-то что возвращает worker и сохраняет connection // почти сделано
-//исключение не должно завершать программу // нет
-//вынести цикл // есть
-//сделать что-то, что обрабатывает одну запись // есть
-//обрабатывать по 10000 //есть
-
-void execute(pqxx::work& worker, pqxx::work& zipWorker)
+void execute()
 {
+    std::string host = "localhost";
+    std::string port = "5432";
+    std::string dbname = "ngp";
+    std::string user = "ngp";
+    std::string zipDbName = "ngpNew";
+    std::string password = "123456";
+
+    PConnection conn(host, port, dbname, user, password);
+    pqxx::work worker = conn.getWorker();
+
+    PConnection zipConn(host, port, zipDbName, user, password);
+    pqxx::work zipWorker = zipConn.getWorker();
+
     //счетчик 
     zipWorker.conn().prepare("insert", "INSERT INTO t_event (type, subjects, timestamp, zip_event, ts_vector) VALUES($1, $2, $3, $4, $5);");
     int id = 0;
@@ -52,22 +59,9 @@ void execute(pqxx::work& worker, pqxx::work& zipWorker)
 
 int main()
 {
-    std::string host = "localhost";
-    std::string port = "5432";
-    std::string dbname = "ngp";
-    std::string user = "ngp";
-    std::string zipDbName = "ngpNew";
-    std::string password = "123456";
     try
     {
-        PConnection conn(host, port, dbname, user, password);
-        pqxx::work worker = conn.getWorker();
-
-        PConnection zipConn(host, port, zipDbName, user, password);
-        pqxx::work zipWorker = zipConn.getWorker();
-
-        execute(worker, zipWorker);
-        zipWorker.commit();
+        execute();
     }
     catch (const std::exception& exc)
     {
